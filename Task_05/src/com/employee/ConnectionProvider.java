b87be781bb39fcb1d308d0240196eed0417da971
Shivam.Shukla <shivamshukla1151@gmail.com>
2023-03-07 18:00:48 +0530

    first commit

--- Task_05/src/com/employee/ConnectionProvider.java
+++ Task_05/src/com/employee/ConnectionProvider.java
@@ -4,29 +4,28 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionProvider {
-	
+
 	static Connection con;
-	
-public static Connection createConnection() {
-	
-	try {
-		
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		
-		String url = "jdbc:mysql://localhost:3306/employee";
-		String user = "root";
-		String password = "admin";
-		
-		con = DriverManager.getConnection(url, user, password);
-		
-	} catch (Exception e) {
 
-		System.out.println("Error Occured in the Connection...!!!");
-		e.printStackTrace();
+	public static Connection createConnection() {
+
+		try {
+
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			String url = "jdbc:mysql://localhost:3306/employee";
+			String user = "root";
+			String password = "admin";
+
+			con = DriverManager.getConnection(url, user, password);
+
+		} catch (Exception e) {
+
+			System.out.println("Error Occured in the Connection...!!!");
+			e.printStackTrace();
+		}
+
+		return con;
 	}
-	
-	return con;
-}
-
 
 }
